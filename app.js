336dc6ccfe785e4c728bc4309516c8113bec93b3
Hannah Baker <hannahb@cogapp.com>
2023-04-14 14:41:04 +0100

    Show either form or 404 randomly

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+let viewer;
+let storedArtworks = {};
+const title = document.getElementById("title");
+const title2 = document.getElementById("title2");
 // Get the radio button and text input element
 const radioYes = document.getElementById("yes");
 const radioNo = document.getElementById("no");
 const captionLabel = document.getElementById("caption-label");
 const caption = document.getElementById("caption");
-const title = document.getElementById("title");
+const captionDisplay = document.getElementById("caption-display");
 const artTitle = document.getElementById("artwork-title");
 const artID = document.getElementById("artwork-id");
 const artImgID = document.getElementById("artwork-image-id");
-const container = document.getElementById("container");
 const submit = document.getElementById("submit");
-let viewer;
 
 
 // Get the form element
@@ -18,8 +20,6 @@ let form = document.querySelector('#form');
     // Get all of the fields in the form
     let fields = form.elements;
     console.log(fields.yes.checked);
-
-let storedArtworks = {};
 
 // Add event listener to the radio button
 radioYes.addEventListener("click", function () {
@@ -43,7 +43,10 @@ radioNo.addEventListener("click", function () {
 async function getArtwork () {
     try {
         // Get current storedArtworks
-        storedArtworks = JSON.parse(localStorage.getItem('approvedArtworks'));
+        if (localStorage.getItem('approvedArtworks')) {
+            storedArtworks = JSON.parse(localStorage.getItem('approvedArtworks'));
+        }
+        
 
         // Run an API call 
         // Create an empty variable to store the response in
@@ -63,14 +66,18 @@ async function getArtwork () {
             let random = data.data[Math.floor(Math.random()*data.data.length)];
 
             // Check that the image_id isn't the archive image and make sure the ID isn't already a key in storedArtworks
-            if (random.image_id != '342b2214-04d5-de63-b577-55a08a618960' && !storedArtworks.hasOwnProperty(random.id)) {
+            if (random.image_id != '342b2214-04d5-de63-b577-55a08a618960' && storedArtworks != null) {
+                if (!storedArtworks.hasOwnProperty(random.id)) {
+                    return random;
+                }
+            } else if (random.image_id != '342b2214-04d5-de63-b577-55a08a618960' && storedArtworks == null) {
                 return random;
             } else {
                 return randomArtwork();
             }
         }
 
-       let artwork = randomArtwork();
+        let artwork = randomArtwork();
 
         console.log(artwork);
 
@@ -124,8 +131,84 @@ async function getArtwork () {
         return viewer;
 
     } catch (error) {
-        container.textContent = '[Something went wrong, sorry!]';
+        document.getElementById("container-form").textContent = '[Something went wrong, sorry!]';
         console.warn(error);
+    }
+}
+
+function getStoredArtwork() {
+    // Get current storedArtworks
+    let keys;
+    let randomIndex;
+    let randomKey;
+    let artwork;
+
+    if (localStorage.getItem('approvedArtworks')) {
+        storedArtworks = JSON.parse(localStorage.getItem('approvedArtworks'));
+
+        // Convert object keys to an array
+        keys = Object.keys(storedArtworks);
+
+        // Generate a random index
+        randomIndex = Math.floor(Math.random() * keys.length);
+
+        // Get the randomly selected key
+        randomKey = keys[randomIndex];
+
+        // Get the value associated with the randomly selected key
+        artwork = storedArtworks[randomKey];
+    }
+
+    if (!artwork) {
+        console.log('no artwork');
+        location.reload();
+    } else {
+        let manifestUrl = 'https://api.artic.edu/api/v1/artworks/' + artwork.id + '/manifest.json';
+
+        // Fetch the IIIF manifest
+        fetch(manifestUrl)
+        .then(response => response.json())
+        .then(manifest => {
+            // Extract the image URL and tile information from the manifest
+            var imageUrl = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["@id"];
+            var tileWidth = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["width"];
+            var tileHeight = manifest["sequences"][0]["canvases"][0]["images"][0]["resource"]["height"];
+            
+            // Construct the OpenSeadragon tileSources object
+            var tileSources = [{
+                type: 'image',
+                url: imageUrl,
+                buildPyramid: false,
+                tileSize: tileWidth,
+                tileOverlap: 0,
+                width: tileWidth,
+                height: tileHeight
+            }];
+    
+            // Create the OpenSeadragon viewer with the IIIF manifest as the tile source
+            viewer = OpenSeadragon({
+                id: 'openseadragon2',
+                prefixUrl: '/openseadragon/images/',
+                crossOriginPolicy: 'Anonymous',
+                showSequenceControl: false,
+                showHomeControl: false,
+                showZoomControl: false,
+                showFullPageControl: false,
+                visibilityRatio: 0.3,
+                homeFillsViewer: false,
+                autoHideControls: true,
+                showNavigator: true,
+                navigatorPosition: 'TOP_LEFT',
+                navigatorAutoFade: true,
+                tileSources: tileSources
+            });
+        })
+        .catch(error => console.error(error));
+    
+        title2.textContent = artwork.title;
+        captionDisplay.textContent = artwork.caption;
+    
+        return viewer;
     }
 }
 
@@ -174,10 +257,30 @@ function storeArtwork() {
     }
 }
 
-// On page load, get a random artwork 
-document.addEventListener('DOMContentLoaded', getArtwork);
+window.onload = function() {
+    // Generate a random number (0 or 1)
+    const randomNum = Math.floor(Math.random() * 2);
 
-form.addEventListener('submit', storeArtwork);
+    // Show/hide pages based on the random number
+    if (randomNum === 0) {
+        document.getElementById("container-form").style.display = "block";
+        document.getElementById("container-404").style.display = "none";
+
+        // On page load, get a random artwork 
+        getArtwork();
+
+        form.addEventListener('submit', storeArtwork);
+
+    } else {
+        document.getElementById("container-form").style.display = "none";
+        document.getElementById("container-404").style.display = "block";
+
+        // On page load, get an approved artwork 
+        getStoredArtwork();
+    }
+  };
+
+
 
 
 
